2f1b35c0eb570f8cf716ea5aa39e11c439a69923
Mark Cruz <mark@Marks-MBP.home>
2020-05-27 11:21:20 -0400

    typo fix

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
             <p className="contact-wrapper__text">
-              Care to learn more I'm alway happy to connect! <br/>  You can reach me at cruzm430@gmail.com
+              Care to learn more I'm always happy to connect! <br/>  You can reach me at cruzm430@gmail.com
             </p>
             {/* <a
               target="_blank"
